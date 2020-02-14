21fdf84eeeae3a853795cf576c66607d60cb2a04
shahbajjamil <shahbajjamil2014@gmail.com>
2020-02-14 23:33:33 +0530

    text  change

--- lib/pages/status.dart
+++ lib/pages/status.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 class Status extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('status screen'),
+    return Center(child: Text('status screen\n comming soon'),
       
     );
   }
